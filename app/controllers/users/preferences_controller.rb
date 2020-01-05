4b823763e599b320fc8ab5ae49ca9fec4f8b4578
Mary Lang <mcl86@cornell.edu>
2020-01-05 16:30:15 -0700

    refactor

--- app/controllers/users/preferences_controller.rb
+++ app/controllers/users/preferences_controller.rb
@@ -28,6 +28,8 @@ class Users::PreferencesController < ApplicationController
     feeling_params.values.each do |feel|
       current_user.feeling_preferences.create(feeling: feel)
     end
+  end
+
 
   def update_music_preferences
     music_params.values.each do |gen|
@@ -35,6 +37,8 @@ class Users::PreferencesController < ApplicationController
         current_user.music_preferences.create(genre: gen)
       end
     end
+  end
+
 
   def update_activity_preferences
     activity_params.values.each do |activity|
@@ -42,41 +46,46 @@ class Users::PreferencesController < ApplicationController
         current_user.activity_preferences.create(description: activity)
       end
     end
+    resource_params.values.each do |resource|
+     current_user.activity_preferences.create(description: resource)
+   end
+  end
 
   def update_time_preferences
     time = TimePreference.create(user_id: current_user.id)
     time_params.values.each do |pref|
       time.update_attribute(pref, true)
     end
+  end
 
   def update_media_preferences
     media_preference = MediaPreference.create(user_id: current_user.id)
     media_params.values.each do |media|
       media_preference.update_attribute(media, true)
     end
+  end
 
-    def music_params
-      params.require('music').permit!
-    end
+  def music_params
+    params.require('music').permit!
+  end
 
-    def feeling_params
-      params.require('feelings').permit!
-    end
+  def feeling_params
+    params.require('feelings').permit!
+  end
 
-    def activity_params
-      params.require('activity').permit!
-    end
+  def activity_params
+    params.require('activity').permit!
+  end
 
-    def resource_params
-      params.require('resources').permit!
-    end
+  def resource_params
+    params.require('resources').permit!
+  end
 
-    def time_params
-      params.require('time').permit!
-    end
+  def time_params
+    params.require('time').permit!
+  end
 
-    def media_params
-      params.require('media').permit!
-    end
-  end 
+  def media_params
+    params.require('media').permit!
+  end
 end
