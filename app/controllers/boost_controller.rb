42536b77c99f1e7f661f0a27d756b0c062bcb7dd
Mary Lang <mcl86@cornell.edu>
2020-01-10 10:12:59 -0700

    Change redirect

--- app/controllers/boost_controller.rb
+++ app/controllers/boost_controller.rb
@@ -12,16 +12,16 @@ class BoostController < ApplicationController
         flash[:error] = 'Please sign-in to a spotify premium account to get a song suggestion'
         redirect_to '/landing'
       else
-        facade = ResourceFacade.new(current_user)
-        facade.get_resource
-        session[:suggestion] = facade.suggestion
-          if session[:suggestion] == 'buddy' && current_user.buddies.empty?
+          if 'buddy' && current_user.buddies.empty? == current_user.resource_preference[:buddy] == true
             flash[:error] = 'Pleaese add a buddy to get a buddy suggestion'
             redirect_to '/buddies/new'
           else
+            facade = ResourceFacade.new(current_user)
+            facade.get_resource
+            session[:suggestion] = facade.suggestion
             render locals: { resource_facade: facade }
         end
-      end 
+      end
     end
   end
 end
