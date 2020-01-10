f8f2783cafe551d27a3a8f061596bf121e546119
Mary Lang <mcl86@cornell.edu>
2020-01-10 10:13:47 -0700

    refactor

--- app/controllers/boost_controller.rb
+++ app/controllers/boost_controller.rb
@@ -11,16 +11,14 @@ class BoostController < ApplicationController
       if current_user.spotify_token == nil && current_user.resource_preference[:music] == true
         flash[:error] = 'Please sign-in to a spotify premium account to get a song suggestion'
         redirect_to '/landing'
+      elsif 'buddy' && current_user.buddies.empty? == current_user.resource_preference[:buddy] == true
+        flash[:error] = 'Pleaese add a buddy to get a buddy suggestion'
+        redirect_to '/buddies/new'
       else
-          if 'buddy' && current_user.buddies.empty? == current_user.resource_preference[:buddy] == true
-            flash[:error] = 'Pleaese add a buddy to get a buddy suggestion'
-            redirect_to '/buddies/new'
-          else
-            facade = ResourceFacade.new(current_user)
-            facade.get_resource
-            session[:suggestion] = facade.suggestion
-            render locals: { resource_facade: facade }
-        end
+        facade = ResourceFacade.new(current_user)
+        facade.get_resource
+        session[:suggestion] = facade.suggestion
+        render locals: { resource_facade: facade }
       end
     end
   end
