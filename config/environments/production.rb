20d5a5e2d97f62800c18649cfbe9f1fb592b0b1c
Mary Lang <mcl86@cornell.edu>
2020-02-18 09:41:59 -0700

    CDN configure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.action_controller.asset_host = "d24cy9j3scl7xa.cloudfront.net"
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
